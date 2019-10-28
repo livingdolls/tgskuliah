31d641535d8c854acfc42b6bfc62f712f6eaadc3
livingdolls <nanang yorozuya@gmail.com>
2019-10-28 16:58:18 +0700

    FINAL

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <div class="menu">
+            <h1>Main Menu</h1>
+            <ul>
+                <li>Data</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
